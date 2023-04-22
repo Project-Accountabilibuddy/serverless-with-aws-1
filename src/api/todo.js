75b8cb44aeb83d084184eead6bb4b7013405ffa8
nfray <harry.fray7@gmail.com>
2023-04-22 10:29:14 -0600

    break out auth reducer

--- src/api/todo.js
+++ src/api/todo.js
@@ -1,8 +1,26 @@
 module.exports.handler = (event, ctx, done) => {
+  console.log(event.pathParameters);
+
+  console.log("GET SPECIFIC TODO: ", event);
+
+  const myTodos = [
+    { id: 1, name: "clean up", status: "open" },
+    { id: 2, name: "cook", status: "done" },
+    { id: 3, name: "party", status: "open" },
+  ];
+
+  const getTodo = myTodos.find(
+    (todo) => todo.id === parseInt(event.pathParameters.id)
+  );
+
   done(null, {
     statusCode: 200,
-    body: JSON.stringify({data: {
-      id: 1, name: 'clean up', status: 'open'
-    }})
-  })
-}
+    body: JSON.stringify({
+      data: {
+        id: 1,
+        name: "clean up",
+        status: "open",
+      },
+    }),
+  });
+};
--- src/api/todos.js
+++ src/api/todos.js
@@ -1,10 +1,12 @@
 module.exports.handler = (event, ctx, done) => {
+  console.log("GET ALL TODOS: ", event);
+
   const todos = [
-    {id: 1, name: 'clean up', status: 'open'},
-    {id: 1, name: 'cook', status: 'done'}
-  ]
+    { id: 1, name: "clean up", status: "open" },
+    { id: 1, name: "cook", status: "done" },
+  ];
   done(null, {
     statusCode: 200,
-    body: JSON.stringify({data: todos})
-  })
-}
+    body: JSON.stringify({ data: todos }),
+  });
+};
